79e38f1583436455f27dda03e4bbf0b741915e25
inigo10rodri <inigo10rodri@gmail.com>
2017-10-10 03:51:33 -0400

    New layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">Buscador de productos para empresas e industrias.</div>
+        <div class="panel-body"></div>
+            <form>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+    </div>
+
+
+@endsection
