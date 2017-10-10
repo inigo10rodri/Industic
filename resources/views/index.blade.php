ee5c8f07c0a898ceaa28ba253cb50bb77ed5d6c1
inigo10rodri <inigo10rodri@gmail.com>
2017-10-10 07:57:26 -0400

    navbar form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,16 +2,6 @@
 
 @section('content')
 
-    <div class="panel panel-primary">
-        <div class="panel-heading">Buscador de productos para empresas e industrias.</div>
-        <div class="panel-body"></div>
-            <form>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-    </div>
 
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,7 @@
         <!-- Styles -->
     </head>
     <body>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-default">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
@@ -25,20 +25,55 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="#">Industic</a>
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+                <form class="navbar-form navbar-right" >
+                    <div class="form-group">
+                        <label ><span class="glyphicon glyphicon-user"></span>Email:</label>
+                        <input type="email"  placeholder="Enter email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label ><span class="glyphicon glyphicon-lock"></span>Password:</label>
+                        <input type="password"   placeholder="Enter password" name="pwd">
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="remember"> Remember me</label>
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
             </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
-    <div>
+    <header class="masthead">
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="homeHeading">Your Favorite Source of Free Bootstrap Themes</h1>
+                <hr>
+                <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+                <a class="btn btn-primary btn-xl js-scroll-trigger" >Find Out More</a>
+            </div>
+        </div>
+    </header>
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <h2 class="section-heading text-white">We've got what you need!</h2>
+                    <hr class="light">
+                    <p class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
+                    <a class="btn btn-default btn-xl js-scroll-trigger" >Get Started!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
         @yield('content')
-    </div>
+    </section>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     </body>
